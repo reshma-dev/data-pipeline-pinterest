9a74764490252cd7d6ebef7b73f3bf4973a690dd
reshma-dev <reshma.sahasrabudhe@gmail.com>
2024-01-25 16:04:07 +0000

    refactor: move send_data code into base class

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -11,19 +11,33 @@ aws_dbconnector = AWSDBConnector("db_creds.yaml")
 
 class PostingEmulation(ABC):
 
-    def __init__(self, destination_list):
+    def __init__(self, destination_list, headers, method, url):
         self.destination_list = destination_list
+        self.headers = headers
+        self.method = method
+        self.invoke_url_template = url
 
     @abstractmethod
-    def send_data(self):
+    def prepare_payload(self):
         pass
+
+    def send_data(self, data, destination):
+        payload = self.prepare_payload(data, destination)
+
+        url = self.invoke_url_template.format(destination=destination)
+
+        response = requests.request(self.method, url, headers=self.headers, data=payload)
+        if response.status_code != 200:
+            print(f"{response.status_code} : {response}")
+        else:
+            print(f"Data sent to {destination}")
 
     def send_one_post(self):
         """Fetch data from the 3 tables on the RDS instance and
         send it to the corresponding Data streams on Kinesis or topics on the MSK
         """
-        for item in zip(aws_dbconnector.fetch_post_data(), self.destination_list):
-            self.send_data(item[0], item[1])
+        for data, destination in zip(aws_dbconnector.fetch_post_data(), self.destination_list):
+            self.send_data(data, destination)
 
 
     def run_infinite_post_data_loop(self):
@@ -43,66 +57,40 @@ class PostingEmulation(ABC):
 
 
 class BatchEmulation(PostingEmulation):
+    
+    # Data destination - Kafka topics in MSK
+    KAFKA_TOPICS = ['12e371d757c1.pin', '12e371d757c1.geo', '12e371d757c1.user']
 
     def __init__(self):
-        # Data destination - Kafka topics in MSK
-        self.destination_list = ['12e371d757c1.pin', '12e371d757c1.geo', '12e371d757c1.user']
+        self.destination_list = self.KAFKA_TOPICS
+        self.headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+        self.method = "POST"
+        self.invoke_url_template = r"https://hh632okm50.execute-api.us-east-1.amazonaws.com/prod/topics/{destination}/"
 
-
-    def send_data(self, data:dict, topic:str):
-        """Send data to kafka topic
-
-        Arguments:
-            data -- topic's data as dictionary
-            topic -- name of topic to send data to
-        """
-        payload = json.dumps({
+    def prepare_payload(self, data:dict, topic):
+        return json.dumps({
             "records": [{ "value": data }]
         }, default=lambda d: d.isoformat())  # convert datetime to serializable isoformat
-
-        # Invoke URL for the API deployed in API Gateway to send data to MSK cluster 
-        invoke_url = r"https://hh632okm50.execute-api.us-east-1.amazonaws.com/prod/topics/{topic}/"
-        headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-
-        url = invoke_url.format(topic=topic)
-        response = requests.request("POST", url, headers=headers, data=payload)
-        if response.status_code != 200:
-            print(f"{response.status_code} : {response}")
-        else:
-            print(f"Data sent to topic {topic}")
 
 
 class StreamEmulation(PostingEmulation):
 
+    # Data destination - data streams in Kinesis
+    DATA_STREAMS = ['streaming-12e371d757c1-pin', 'streaming-12e371d757c1-geo', 'streaming-12e371d757c1-user']
+    
     def __init__(self):
-        # Data destination - data streams in Kinesis
-        self.destination_list = ['streaming-12e371d757c1-pin', 'streaming-12e371d757c1-geo', 'streaming-12e371d757c1-user']
-        
-    def send_data(self, data:dict, stream_name:str):
-        """Send data to stream
+        self.destination_list = self.DATA_STREAMS
+        self.headers = {'Content-Type': 'application/json'}
+        self.method = "PUT"
+        self.invoke_url_template = r"https://hh632okm50.execute-api.us-east-1.amazonaws.com/prod/streams/{destination}/record"
 
-        Arguments:
-            data -- stream's data as dictionary
-            stream_name -- data stream name in Amazon Kinesis to which data is being sent
-        """
-        # invoke url format for one record
-        # "https://<APIInvokeURL>/<DeploymentStage>/streams/<stream_name>/record"
-        
-        invoke_url='https://hh632okm50.execute-api.us-east-1.amazonaws.com/prod/streams/'
-        headers = {'Content-Type': 'application/json'}
-        
-        data = json.dumps({
+    def prepare_payload(self, data, stream_name):
+        return json.dumps({
             "StreamName": stream_name,
             "Data": data,
             "PartitionKey": "partition-1"
         }, default=lambda d: d.isoformat())  # convert datetime to serializable isoformat
 
-        response = requests.request("PUT", invoke_url + stream_name + "/record", headers=headers, data=data)
-
-        if response.status_code != 200:
-            print(f"{response.status_code} : {response.content}")
-        else:
-            print(f"Data sent to stream {stream_name} - {response.content}")
 
 if __name__ == "__main__":
     b = BatchEmulation()
